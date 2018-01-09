e79872620867976ab6790bf83941317e656116f0
Kay Miller <i@ikay.work>
2018-01-09 14:36:22 -0800

    Fix error

--- 2018-01-Puzzles/01-Rabbits.py
+++ 2018-01-Puzzles/01-Rabbits.py
@@ -8,4 +8,4 @@ def rabbits(time, month, old_ones, newborns):
         print("Month:", month, " total:", old_ones + newborns)
         rabbits(time, month + 1, old_ones + newborns, old_ones)
 
-rabbits(12, 1, 0, 1)
+rabbits(12, 0, 0, 1)
